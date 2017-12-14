0b88f37ab5e6586091bd8f410cf80e61ac2d73d6
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2017-12-14 12:45:10 +0100

    All CRUD methodes implemented

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -4,11 +4,19 @@ import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
 
-    Task findById(long taskId);
+    @Override
+    Optional<Task> findById(Long id);
+
+    @Override
+    Task save(Task task);
+
+    @Override
+    void deleteById(Long id);
 }
