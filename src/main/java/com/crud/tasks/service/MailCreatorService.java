0beaccc6922554ad8290bb8e2b7349e96b49ac3f
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2018-02-17 23:55:35 +0100

    Thymeleaf - scheduled email template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -2,14 +2,20 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.config.CompanyDetails;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
+    @Autowired
+    private TaskRepository taskRepository;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -22,6 +28,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("preview", "Trello app - new card added");
         context.setVariable("message", message);
@@ -31,6 +43,37 @@ public class MailCreatorService {
         context.setVariable("company_name", companyDetails.getAppName());
         context.setVariable("company_details", companyDetails.getAppName() + "\n" +
                 companyDetails.getOwnerName() + " " + companyDetails.getOwnerSurname());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildScheduledEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
+        long size = taskRepository.count();
+        String taskOrTasks = size == 1 ? " task" : " tasks";
+        message = "Currently in database you've got: " + size + taskOrTasks;
+
+        Context context = new Context();
+        context.setVariable("preview", "Trello app - Your daily information");
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
+        context.setVariable("button", "See tasks");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", companyDetails.getAppName());
+        context.setVariable("company_details", companyDetails.getAppName() + "\n" +
+                companyDetails.getOwnerName() + " " + companyDetails.getOwnerSurname());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
